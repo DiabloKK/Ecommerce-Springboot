6791622b8ee6e2af7e641407dd458a7d92cedaf4
NoIr <vokhuong@gmail.com>
2023-09-22 10:15:26 +0700

    Finished Unit 9

--- Library/src/main/java/com/ecommerce/library/service/impl/ProductServiceImpl.java
+++ Library/src/main/java/com/ecommerce/library/service/impl/ProductServiceImpl.java
@@ -6,6 +6,9 @@ import com.ecommerce.library.repository.ProductRepository;
 import com.ecommerce.library.service.ProductService;
 import com.ecommerce.library.utils.ImageUpload;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -124,5 +127,19 @@ public class ProductServiceImpl implements ProductService {
         productDto.setDeleted(product.is_deleted());
         productDto.setActivated(product.is_activated());
         return productDto;
+    }
+
+    @Override
+    public Page<Product> pageProducts(int pageNo) {
+        Pageable paging = PageRequest.of(pageNo, 5);
+        Page<Product> productPages = productRepository.pageProduct(paging);
+        return productPages;
+    }
+
+    @Override
+    public Page<Product> searchProducts(int pageNo, String keyword) {
+        Pageable pageable = PageRequest.of(pageNo, 5);
+        Page<Product> products = productRepository.searchProducts(keyword, pageable);
+        return products;
     }
 }
